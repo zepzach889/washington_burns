6958b8d2aa5446431908030a8d28e26ab06ad12e
zepzach889 <zepzach889@gmail.com>
2026-05-20 19:02:09 -0700

    Update nedex_index.js

--- nodes/nedex/nedex_index.js
+++ nodes/nedex/nedex_index.js
@@ -35,7 +35,9 @@ const NEDEX_ARTICLES = [
   { title: 'James Hawthorne',                      file: 'james-hawthorne.html',    categories: ['Biographies', 'Presidents of the United States', 'Republican Party politicians', 'War Between the States'],     blurb: 'Thirteenth President of the United States (1856–1860); Led the "Slaver Union" during the War Between the States after assuming the presidency upon President Lee\'s assassination. Heavihandedness likely triggerd the War.' },
   { title: 'Hugo Brandt',                          file: 'hugo-brandt.html',        categories: ['Biographies', 'Presidents of the United States', 'Democrat Party politicians', 'War Between the States'],       blurb: 'Fourteenth President of the United States (1863–1869); Acting President of the Free States (1858–1863); led the republic through the War Between the States.' },
   { title: 'John P. Quincy',                       file: 'john-p-quincy.html',      categories: ['Biographies', 'Presidents of the United States', 'Democrat Party politicians', 'Reconstruction'],               blurb: 'Fifteenth President of the United States (1869–1873); Popular Democratic president, seen as the finisher of reconstruction. Resigned in 1873 amid scandal.' },
+  { title: 'Bradley Kinder',                       file: 'bradley-kinder.html',     categories: ['Biographies', 'Presidents of the United States', 'Democrat Party politicians', 'Reconstruction'],               blurb: 'Sixteenth President of the United States (1873–1875); Caretaker president following the resignation of Presideny Quincy in the wake of the Clark Scandal.' },
 
+  
   
 ];
 
